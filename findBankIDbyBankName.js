c4c2699ad764f29b2a7e102fbaa8497f95396b9d
MaiHsunWei <30306218+MaiHsunWei@users.noreply.github.com>
2022-09-13 02:24:26 +0800

    22.09.13.v1

---
+++ findBankIDbyBankName.js
@@ -0,0 +1,88 @@
+ const bankObjs=[
+  {"category":"农村信用社","Banks":[
+          {"bank_id":"BK0031","band_code":"HURCB","type":"String","bank_name":"湖北省农村信用社"},{"bank_id":"BK0039","band_code":"GDRCC","type":"String","bank_name":"广东省农村信用社联合社"},{"bank_id":"BK0044","band_code":"GZRCU","type":"String","bank_name":"贵州省农村信用社"},{"bank_id":"BK0047","band_code":"JLRCU","type":"String","bank_name":"吉林农信"},{"bank_id":"BK0050","band_code":"SDRCU","type":"String","bank_name":"山东农信"},{"bank_id":"BK0053","band_code":"GXRCU","type":"String","bank_name":"广西省农村信用"},{"bank_id":"BK0067","band_code":"HNRCC","type":"String","bank_name":"湖南省农村信用社"},{"bank_id":"BK0070","band_code":"CZRCB","type":"String","bank_name":"常州农村信用联社"},{"bank_id":"BK0084","band_code":"SCRCU","type":"String","bank_name":"四川省农村信用"},{"bank_id":"BK0085","band_code":"HBRCU","type":"String","bank_name":"河北省农村信用社"},{"bank_id":"BK0090","band_code":"HNRCU","type":"String","bank_name":"河南省农村信用"},{"bank_id":"BK0091","band_code":"YNRCC","type":"String","bank_name":"云南省农村信用社"},{"bank_id":"BK0095","band_code":"JSRCU","type":"String","bank_name":"江苏省农村信用联合社"},{"bank_id":"BK0104","band_code":"JXRCU","type":"String","bank_name":"江西省农村信用"},{"bank_id":"BK0111","band_code":"NHB","type":"String","bank_name":"南海农村信用联社"},{"bank_id":"BK0115","band_code":"NHQS","type":"String","bank_name":"农信银清算中心"},{"bank_id":"BK0133","band_code":"ARCU","type":"String","bank_name":"安徽省农村信用社"},{"bank_id":"BK0138","band_code":"GSRCU","type":"String","bank_name":"甘肃省农村信用"},{"bank_id":"BK0139","band_code":"ZJNX","type":"String","bank_name":"浙江省农村信用社联合社"},{"bank_id":"BK0167","band_code":"FJNX","type":"String","bank_name":"福建农村信用社"},{"bank_id":"BK0185","band_code":"HNRCB","type":"String","bank_name":"海南省农村信用社联合社"},{"bank_id":"BK0189","band_code":"YACCB","type":"String","bank_name":"山西省农村信用社联合社"},{"bank_id":"BK0190","band_code":"HLJRCC","type":"String","bank_name":"黑龙江农村信用社"},{"bank_id":"BK0191","band_code":"NMGNXS","type":"String","bank_name":"内蒙古农村信用社"}]},
+  {"category":"农村商业银行","Banks":[
+         {"bank_id":"BK0018","band_code":"SRCB","type":"String","bank_name":"深圳农村商业银行"},{"bank_id":"BK0024","band_code":"SHRCB","type":"String","bank_name":"上海农村商业银行"},{"bank_id":"BK0028","band_code":"CSRCB","type":"String","bank_name":"常熟农村商业银行"},{"bank_id":"BK0030","band_code":"SDEB","type":"String","bank_name":"顺德农商银行"},{"bank_id":"BK0032","band_code":"WRCB","type":"String","bank_name":"无锡农村商业银行"},{"bank_id":"BK0054","band_code":"NXRCU","type":"String","bank_name":"宁夏黄河农村商业银行"},{"bank_id":"BK0058","band_code":"GRCB","type":"String","bank_name":"广州农商银行"},{"bank_id":"BK0077","band_code":"TCRCB","type":"String","bank_name":"江苏太仓农村商业银行"},{"bank_id":"BK0089","band_code":"YDRCB","type":"String","bank_name":"尧都农商行"},{"bank_id":"BK0101","band_code":"WHRCB","type":"String","bank_name":"武汉农村商业银行"},{"bank_id":"BK0105","band_code":"ZRCBANK","type":"String","bank_name":"张家港农村商业银行"},{"bank_id":"BK0116","band_code":"JRCB","type":"String","bank_name":"江苏江阴农村商业银行"},{"bank_id":"BK0124","band_code":"DRCBCL","type":"String","bank_name":"东莞农村商业银行"},{"bank_id":"BK0128","band_code":"CRCBANK","type":"String","bank_name":"重庆农村商业银行"},{"bank_id":"BK0130","band_code":"KSRB","type":"String","bank_name":"昆山农村商业银行"},{"bank_id":"BK0135","band_code":"CDRCB","type":"String","bank_name":"成都农商银行"},{"bank_id":"BK0142","band_code":"TRCB","type":"String","bank_name":"天津农商银行"},{"bank_id":"BK0146","band_code":"WJRCB","type":"String","bank_name":"吴江农商银行"},{"bank_id":"BK0159","band_code":"BJRCB","type":"String","bank_name":"北京农村商业银行"}]},
+  {"category":"商业银行","Banks":[
+       {"bank_id":"BK0025","band_code":"WHCCB","type":"String","bank_name":"威海市商业银行"},{"bank_id":"BK0029","band_code":"KORLABANK","type":"String","bank_name":"库尔勒市商业银行"},{"bank_id":"BK0036","band_code":"TACCB","type":"String","bank_name":"泰安市商业银行"},{"bank_id":"BK0038","band_code":"LYCB","type":"String","bank_name":"辽阳市商业银行"},{"bank_id":"BK0043","band_code":"CZCB","type":"String","bank_name":"浙江稠州商业银行"},{"bank_id":"BK0045","band_code":"ZJKCCB","type":"String","bank_name":"张家口市商业银行"},{"bank_id":"BK0065","band_code":"ZYCBANK","type":"String","bank_name":"遵义市商业银行"},{"bank_id":"BK0069","band_code":"DYCCB","type":"String","bank_name":"东营市商业银行"},{"bank_id":"BK0074","band_code":"ZGCCB","type":"String","bank_name":"自贡市商业银行"},{"bank_id":"BK0081","band_code":"DYCB","type":"String","bank_name":"德阳商业银行"},{"bank_id":"BK0086","band_code":"LSCCB","type":"String","bank_name":"乐山市商业银行"},{"bank_id":"BK0088","band_code":"CBKF","type":"String","bank_name":"开封市商业银行"},{"bank_id":"BK0093","band_code":"YXCCB","type":"String","bank_name":"玉溪市商业银行"},{"bank_id":"BK0107","band_code":"JZBANK","type":"String","bank_name":"晋中市商业银行"},{"bank_id":"BK0117","band_code":"HZCCB","type":"String","bank_name":"湖州市商业银行"},{"bank_id":"BK0118","band_code":"MTBANK","type":"String","bank_name":"浙江民泰商业银行"},{"bank_id":"BK0143","band_code":"URMQCCB","type":"String","bank_name":"乌鲁木齐市商业银行"},{"bank_id":"BK0148","band_code":"YBCCB","type":"String","bank_name":"宜宾市商业银行"},{"bank_id":"BK0149","band_code":"ZJTLCB","type":"String","bank_name":"浙江泰隆商业银行"},{"bank_id":"BK0156","band_code":"GYCB","type":"String","bank_name":"贵阳市商业银行"},{"bank_id":"BK0163","band_code":"CGNB","type":"String","bank_name":"南充市商业银行"},{"bank_id":"BK0164","band_code":"CBBQS","type":"String","bank_name":"城市商业银行资金清算中心"},{"bank_id":"BK0187","band_code":"YACCB","type":"String","bank_name":"雅安市商业银行"}]},
+  {"category":"银行","Banks":[
+         {"bank_id":"BK0001","band_code":"CCB","type":"String","bank_name":"建设银行"},{"bank_id":"BK0002","band_code":"ABC","type":"String","bank_name":"农业银行"},{"bank_id":"BK0003","band_code":"ICBC","type":"String","bank_name":"工商银行"},{"bank_id":"BK0004","band_code":"BOC","type":"String","bank_name":"中国银行"},{"bank_id":"BK0005","band_code":"CMBC","type":"String","bank_name":"中国民生银行"},{"bank_id":"BK0006","band_code":"CMB","type":"String","bank_name":"招商银行"},{"bank_id":"BK0007","band_code":"CIB","type":"String","bank_name":"兴业银行"},{"bank_id":"BK0008","band_code":"BJBANK","type":"String","bank_name":"北京银行"},{"bank_id":"BK0009","band_code":"COMM","type":"String","bank_name":"交通银行"},{"bank_id":"BK0010","band_code":"CEB","type":"String","bank_name":"光大银行"},{"bank_id":"BK0011","band_code":"SPABANK","type":"String","bank_name":"平安银行"},{"bank_id":"BK0012","band_code":"PSBC","type":"String","bank_name":"邮政银行"},{"bank_id":"BK0013","band_code":"CITIC","type":"String","bank_name":"中信银行"},{"bank_id":"BK0014","band_code":"HXBANK","type":"String","bank_name":"华夏银行"},{"bank_id":"BK0015","band_code":"GCB","type":"String","bank_name":"广州银行"},{"bank_id":"BK0016","band_code":"SPDB","type":"String","bank_name":"上海浦东发展银行"},{"bank_id":"BK0017","band_code":"GDB","type":"String","bank_name":"广发银行"},{"bank_id":"BK0019","band_code":"SCCB","type":"String","bank_name":"三门峡银行"},{"bank_id":"BK0020","band_code":"BGB","type":"String","bank_name":"广西北部湾银行"},{"bank_id":"BK0021","band_code":"YQCCB","type":"String","bank_name":"阳泉银行"},{"bank_id":"BK0022","band_code":"SHBANK","type":"String","bank_name":"上海银行"},{"bank_id":"BK0023","band_code":"JLBANK","type":"String","bank_name":"吉林银行"},{"bank_id":"BK0026","band_code":"BANKWF","type":"String","bank_name":"潍坊银行"},{"bank_id":"BK0027","band_code":"BOZK","type":"String","bank_name":"周口银行"},{"bank_id":"BK0033","band_code":"BOCY","type":"String","bank_name":"朝阳银行"},{"bank_id":"BK0034","band_code":"CZBANK","type":"String","bank_name":"浙商银行"},{"bank_id":"BK0035","band_code":"HDBANK","type":"String","bank_name":"邯郸银行"},{"bank_id":"BK0037","band_code":"BOD","type":"String","bank_name":"东莞银行"},{"bank_id":"BK0040","band_code":"LZYH","type":"String","bank_name":"兰州银行"},{"bank_id":"BK0041","band_code":"SXCB","type":"String","bank_name":"绍兴银行"},{"bank_id":"BK0042","band_code":"BOHAIB","type":"String","bank_name":"渤海银行"},{"bank_id":"BK0046","band_code":"BOJZ","type":"String","bank_name":"锦州银行"},{"bank_id":"BK0048","band_code":"BOP","type":"String","bank_name":"平顶山银行"},{"bank_id":"BK0049","band_code":"SRBANK","type":"String","bank_name":"上饶银行"},{"bank_id":"BK0051","band_code":"SJBANK","type":"String","bank_name":"盛京银行"},{"bank_id":"BK0052","band_code":"HKB","type":"String","bank_name":"汉口银行"},{"bank_id":"BK0055","band_code":"BSB","type":"String","bank_name":"包商银行"},{"bank_id":"BK0056","band_code":"JSBANK","type":"String","bank_name":"江苏银行"},{"bank_id":"BK0057","band_code":"NYNB","type":"String","bank_name":"广东南粤银行"},{"bank_id":"BK0059","band_code":"BOSZ","type":"String","bank_name":"苏州银行"},{"bank_id":"BK0060","band_code":"HZCB","type":"String","bank_name":"杭州银行"},{"bank_id":"BK0061","band_code":"DZBANK","type":"String","bank_name":"德州银行"},{"bank_id":"BK0062","band_code":"ORBANK","type":"String","bank_name":"鄂尔多斯银行"},{"bank_id":"BK0063","band_code":"HBC","type":"String","bank_name":"湖北银行"},{"bank_id":"BK0064","band_code":"JXBANK","type":"String","bank_name":"嘉兴银行"},{"bank_id":"BK0066","band_code":"BODD","type":"String","bank_name":"丹东银行"},{"bank_id":"BK0068","band_code":"AYCB","type":"String","bank_name":"安阳银行"},{"bank_id":"BK0071","band_code":"EGBANK","type":"String","bank_name":"恒丰银行"},{"bank_id":"BK0072","band_code":"CDB","type":"String","bank_name":"国家开发银行"},{"bank_id":"BK0073","band_code":"HSBK","type":"String","bank_name":"衡水银行"},{"bank_id":"BK0075","band_code":"CDCB","type":"String","bank_name":"成都银行"},{"bank_id":"BK0076","band_code":"JNBANK","type":"String","bank_name":"济宁银行"},{"bank_id":"BK0078","band_code":"NJCB","type":"String","bank_name":"南京银行"},{"bank_id":"BK0079","band_code":"ZZBANK","type":"String","bank_name":"郑州银行"},{"bank_id":"BK0080","band_code":"LYBANK","type":"String","bank_name":"洛阳银行"},{"bank_id":"BK0082","band_code":"ZBCB","type":"String","bank_name":"齐商银行"},{"bank_id":"BK0083","band_code":"FSCB","type":"String","bank_name":"抚顺银行"},{"bank_id":"BK0087","band_code":"LSBANK","type":"String","bank_name":"莱商银行"},{"bank_id":"BK0092","band_code":"H3CB","type":"String","bank_name":"内蒙古银行"},{"bank_id":"BK0094","band_code":"FDB","type":"String","bank_name":"富滇银行"},
+      {"bank_id":"BK0096","band_code":"XYBANK","type":"String","bank_name":"信阳银行"},{"bank_id":"BK0097","band_code":"HANABANK","type":"String","bank_name":"韩亚银行"},{"bank_id":"BK0098","band_code":"SZSBK","type":"String","bank_name":"石嘴山银行"},{"bank_id":"BK0099","band_code":"JINCHB","type":"String","bank_name":"晋城银行JCBANK"},{"bank_id":"BK0100","band_code":"FXCB","type":"String","bank_name":"阜新银行"},{"bank_id":"BK0102","band_code":"HBYCBANK","type":"String","bank_name":"湖北银行宜昌分行"},{"bank_id":"BK0103","band_code":"TZCB","type":"String","bank_name":"台州银行"},{"bank_id":"BK0106","band_code":"JSB","type":"String","bank_name":"晋商银行"},{"bank_id":"BK0108","band_code":"FJHXBC","type":"String","bank_name":"福建海峡银行"},{"bank_id":"BK0109","band_code":"XCYH","type":"String","bank_name":"许昌银行"},{"bank_id":"BK0110","band_code":"NXBANK","type":"String","bank_name":"宁夏银行"},{"bank_id":"BK0112","band_code":"XXBANK","type":"String","bank_name":"新乡银行"},{"bank_id":"BK0113","band_code":"HSBANK","type":"String","bank_name":"徽商银行"},{"bank_id":"BK0114","band_code":"JJBANK","type":"String","bank_name":"九江银行"},{"bank_id":"BK0119","band_code":"LANGFB","type":"String","bank_name":"廊坊银行"},{"bank_id":"BK0120","band_code":"ASCB","type":"String","bank_name":"鞍山银行"},{"bank_id":"BK0121","band_code":"SXRCCU","type":"String","bank_name":"陕西信合"},{"bank_id":"BK0122","band_code":"CCQTGB","type":"String","bank_name":"重庆三峡银行"},{"bank_id":"BK0123","band_code":"DLB","type":"String","bank_name":"大连银行"},{"bank_id":"BK0125","band_code":"NBBANK","type":"String","bank_name":"宁波银行"},{"bank_id":"BK0126","band_code":"XABANK","type":"String","bank_name":"西安银行"},{"bank_id":"BK0127","band_code":"KLB","type":"String","bank_name":"昆仑银行"},{"bank_id":"BK0129","band_code":"BOYK","type":"String","bank_name":"营口银行"},{"bank_id":"BK0131","band_code":"HRXJB","type":"String","bank_name":"华融湘江银行"},{"bank_id":"BK0132","band_code":"GLBANK","type":"String","bank_name":"桂林银行"},{"bank_id":"BK0134","band_code":"BOQH","type":"String","bank_name":"青海银行"},{"bank_id":"BK0136","band_code":"QDCCB","type":"String","bank_name":"青岛银行"},{"bank_id":"BK0137","band_code":"HKBEA","type":"String","bank_name":"东亚银行"},{"bank_id":"BK0140","band_code":"HBHSBANK","type":"String","bank_name":"湖北银行黄石分行"},{"bank_id":"BK0141","band_code":"WZCB","type":"String","bank_name":"温州银行"},{"bank_id":"BK0144","band_code":"XLBANK","type":"String","bank_name":"中山小榄村镇银行"},{"bank_id":"BK0145","band_code":"CSCB","type":"String","bank_name":"长沙银行"},{"bank_id":"BK0147","band_code":"QLBANK","type":"String","bank_name":"齐鲁银行"},{"bank_id":"BK0150","band_code":"JHBANK","type":"String","bank_name":"金华银行"},{"bank_id":"BK0151","band_code":"BHB","type":"String","bank_name":"河北银行"},
+      {"bank_id":"BK0152","band_code":"GZB","type":"String","bank_name":"赣州银行"},{"bank_id":"BK0153","band_code":"BZMD","type":"String","bank_name":"驻马店银行"},{"bank_id":"BK0154","band_code":"NBYZ","type":"String","bank_name":"鄞州银行"},{"bank_id":"BK0155","band_code":"LSBC","type":"String","bank_name":"临商银行"},{"bank_id":"BK0157","band_code":"CQBANK","type":"String","bank_name":"重庆银行"},{"bank_id":"BK0158","band_code":"BOCD","type":"String","bank_name":"承德银行"},{"bank_id":"BK0160","band_code":"NCB","type":"String","bank_name":"南昌银行"},{"bank_id":"BK0161","band_code":"DAQINGB","type":"String","bank_name":"龙江银行"},{"bank_id":"BK0162","band_code":"TCCB","type":"String","bank_name":"天津银行"},{"bank_id":"BK0165","band_code":"XTB","type":"String","bank_name":"邢台银行"},{"bank_id":"BK0166","band_code":"XMBANK","type":"String","bank_name":"厦门银行"},{"bank_id":"BK0168","band_code":"XIB","type":"String","bank_name":"厦门国际银行"},{"bank_id":"BK0169","band_code":"HSBC","type":"String","bank_name":"汇丰银行"},{"bank_id":"BK0170","band_code":"CCAB","type":"String","bank_name":"长安银行"},{"bank_id":"BK0171","band_code":"YZB","type":"String","bank_name":"深圳福田银座银行"},{"bank_id":"BK0172","band_code":"CRB","type":"String","bank_name":"珠海滑润银行"},{"bank_id":"BK0173","band_code":"LZCCB","type":"String","bank_name":"柳州银行"},{"bank_id":"BK0174","band_code":"MYB","type":"String","bank_name":"浙江網商銀行"},{"bank_id":"BK0175","band_code":"HRBB","type":"String","bank_name":"哈尔滨银行"},{"bank_id":"BK0176","band_code":"JXB","type":"String","bank_name":"江西银行"},
+      {"bank_id":"BK0177","band_code":"ZYB","type":"String","bank_name":"中原银行"},{"bank_id":"BK0178","band_code":"CCHRCB","type":"String","bank_name":"长春朝阳和润村镇银行"},{"bank_id":"BK0179","band_code":"LJYY","type":"String","bank_name":"龙井榆银村镇银行"},{"bank_id":"BK0180","band_code":"RTCB","type":"String","bank_name":"长春绿园融泰村镇银行"},{"bank_id":"BK0181","band_code":"CCRFCB","type":"String","bank_name":"长春经开融丰村镇银行"},{"bank_id":"BK0182","band_code":"TMDYCZYH","type":"String","bank_name":"图门敦银村镇银行"},{"bank_id":"BK0183","band_code":"YJHRVB","type":"String","bank_name":"延吉和润村镇银行"},{"bank_id":"BK0184","band_code":"BEEB","type":"String","bank_name":"广西银海国民村镇银行"},{"bank_id":"BK0186","band_code":"HNB","type":"String","bank_name":"海南银行"},{"bank_id":"BK0188","band_code":"QZCCB","type":"String","bank_name":"泉州银行"}]}];
+
+
+
+
+function returnBankObj(BankCategory,bankName){
+  console.log(bankName);
+  var BankId="";
+   
+      for(let i=0;i<bankObjs[BankCategory].Banks.length;i++){
+         console.log(bankObjs[BankCategory].Banks[i].bank_name.indexOf(bankName));
+        if(bankObjs[BankCategory].Banks[i].bank_name.indexOf(bankName)!=-1&&bankObjs[BankCategory].Banks[i].bank_name.length>=2){
+          BankId=bankObjs[BankCategory].Banks[i].bank_id;
+        }else if(bankObjs[BankCategory].Banks[i].bank_name.indexOf(bankName)!=-1&&bankName==bankName.slice(0,11)){
+          BankId=bankObjs[BankCategory].Banks[i].bank_id;
+        }
+      
+    };
+  console.log("类型:"+BankCategory+" ","BANK_ID:"+BankId+" /br",bankObjs);
+  return BankId;
+}
+function findBankName(bankname){
+  var ret="";
+  if(bankname.indexOf("农")&&bankname.indexOf("信")){
+    ret = bankname.slice(0,bankname.indexOf("农"));
+  } 
+  else if(bankname.indexOf("农")&&bankname.indexOf("商")){
+    ret = bankname.slice(0,bankname.indexOf("农"));
+  }
+  else if(bankname.indexOf("商")&&bankname.indexOf("银")){
+    ret = bankname.slice(0,bankname.indexOf("商"));
+  }
+  else{
+     var index=0;
+     if(bankname.indexOf("银")!=-1){
+       index=bankname.indexOf("银");
+     }
+     else if(bankname.indexOf("行")!=-1){
+       index=bankname.indexOf("行");
+     }
+     else{
+        ret=bankname;
+     }
+     if(ret.length>2&&ret.indexOf("中国")!=-1){
+       ret = bankname.slice(0,2);
+     }
+     ret = bankname.slice(0,index);
+  }  
+  
+  return ret;
+};
+function findBankCategory(bankname){
+  var ret=0;
+  if(bankname.indexOf("农")!=-1&&bankname.indexOf("信")!=-1){
+    ret = 0;
+  } 
+  else if(bankname.indexOf("农")!=-1&&bankname.indexOf("商")!=-1){
+    ret = 1;
+  }
+  else if(bankname.indexOf("商")!=-1&&bankname.indexOf("银")!=-1&&bankname.length!=4){
+    ret = 2;
+  }
+  else{
+     ret= 3;
+  }  
+  return ret;
+};
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
